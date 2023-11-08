49fbed103b88b01aa26e755433907c1b57ac132b
EdsonCasta <dedsoncasta07@gmail.com>
2023-11-07 22:48:31 -0500

    01-introToCS_homework

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,9 +1,35 @@
 'use strict';
-// commit de prueba
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var reverse = num.split("").reverse().join("");  
+   var base = 2; 
+   var decimal = 0;
+   var suma = 0;
 
-function DecimalABinario(num) {}
+   for(var i = 0; i < reverse.length; i++){
+      suma = reverse[i] * base ** i; 
+      decimal += suma;
+   }
+   return decimal;
+}
+
+console.log(BinarioADecimal("10"));
+
+function DecimalABinario(num) {
+   var binario = ""; 
+   var cociente = 0; 
+   var resto = 0;
+
+   while (num > 0) {
+      cociente = Math.floor(num / 2);
+      resto = num % 2;
+      binario = resto.toString() + binario;
+      num = cociente
+   }
+   return binario;
+}
+
+console.log(DecimalABinario(8));
 
 module.exports = {
    BinarioADecimal,
