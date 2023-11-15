efbebdf81b43df0d4c6796b39e76bdd27d07b573
EdsonCasta <dedsoncasta07@gmail.com>
2023-11-15 16:49:41 -0500

    07-Algorismos-I_homework

--- FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -7,15 +7,42 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  var array = [1];   //32 % 2 = 0   16 %
+  var divisor = 2;   //32 / 2 = 16 
+
+  while (num > 1) {
+    if(num % divisor === 0){
+      array.push(divisor);
+      num /= divisor
+    }else{
+      divisor++;
+    }
+  }
+  return array;
 }
+
+console.log(factorear(32)); // [1, 2, ]
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+let swap;
 
+do {
+  swap = false;
+  for(let i = 0; i< array.length - 1; i++){
+    if(array[i] > array[i + 1]){
+      [array[i], array[i + 1]] = [array[i + 1], array[i]];
+      swap = true
+    }
+  }
+} while (swap);
+return array;
 }
+
+console.log(bubbleSort([10, -2, -7, 4]));
 
 
 function insertionSort(array) {
@@ -23,18 +50,52 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 1; i < array.length; i++){
+    let current = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
+  return array;
 }
 
+console.log(insertionSort([5, 1, 4, 2, 8]));
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  // Implementar el método conocido como selectionSort para ordenar de menor a mayor
+  // el array recibido como parámetro utilizando dos arreglos
+  // Devolver el array ordenado resultante
+  // Tu código:
 
+  let n = array.length; // Obtiene la longitud del array para determinar el límite de las iteraciones.
+
+  // Recorre cada elemento del array, excepto el último.
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i; // Asume que el elemento actual es el mínimo.
+    // Recorre los elementos siguientes al actual.
+    for (let j = i + 1; j < n; j++) {
+      // Si encuentra un elemento menor,
+      if (array[j] < array[minIndex]) {
+        minIndex = j; // actualiza el índice del mínimo.
+      }
+    }
+    // Si el mínimo no es el elemento actual,
+    if (minIndex != i) {
+      [array[i], array[minIndex]] = [array[minIndex], array[i]]; // intercambia los elementos.
+    }
+  }
+
+  return array; // Devuelve el array ordenado.
 }
 
+console.log(selectionSort([5, 1, 4, 2, 8]));
 
 // No modificar nada debajo de esta línea
 // --------------------------------
